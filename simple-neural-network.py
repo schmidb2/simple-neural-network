577f95e4112c8a031e92f1a2e8422a936ddb67f6
Brooklyn Schmidt <brooklyn.schmidt22@gmail.com>
2022-12-03 18:36:34 -0500

    Adding comments to code

--- simple-neural-network.py
+++ simple-neural-network.py
@@ -9,20 +9,28 @@ def sigmoid_derivative(x):
 
 data = np.array([[0,0,1,0],[1,1,1,1],[1,0,1,1],[0,1,1,0]])
 
-x = data[:,0:3]
-y = np.array([data[:,3]]).T
+x = data[:,0:3] #input values
+y = np.array([data[:,3]]).T #output values
 
-random.seed(1)
-synaptic_weights = 2 * random.random((3, 1)) - 1
+random.seed(1) #this saves the randomly generated weights so the starting
+               #weights are the same everytime the program runs
+synaptic_weights = 2 * random.random((3, 1)) - 1 #starting weights range between -1 and 1
 
 num_iterations = 10000
 
 for iteration in range(num_iterations):
     
+    #input value is adjusted by weight using the dot product
     dot_prod = dot(x,synaptic_weights)
+    #the values are normalized with the sigmoid function so the result is between
+    #0 and 1
     output = sigmoid(dot_prod)
+    #comparing the calculated output to the expected output
     error = y - output
+    #the error is multiplied by the gradient of the sigmoid curve then
+    #applied back to the input
     adjustment = dot(x.T, error*sigmoid_derivative(output))
+    #applying our adjustment to update the synaptic weights
     synaptic_weights += adjustment
 
 #testing the results
