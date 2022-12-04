09278da33f5821d432be3b91e0ae3238a663148b
Brooklyn Schmidt <brooklyn.schmidt22@gmail.com>
2022-12-04 15:40:39 -0500

    added graph plotting to code and adding graph outputs

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # simple-neural-network
 A simple neural network without the use of any neural network library.
+
+#### To learn the basics of neural networks, I first started with a simple example found in the blog below. 
+
+https://medium.com/technology-invention-and-more/how-to-build-a-simple-neural-network-in-9-lines-of-python-code-cc8f23647ca1
+
+#### This example uses a small training set of ones and zeros. 
+
+![Training Set](training_set.jpg)
+
+## Training
+
+#### The pattern on this training set is that if the first feature is one, then the output is one. Likewise, if the first feature is zero, then the output is zero. 
+
+#### In this example, each feature will be adjusted by a weight, then passed through the sigmoid function. The larger the weight (positive or negative), the larger the impact of that feature on the output. In this case, since the output is determined by the first feature, the expected outcome is that the weight for the first feature will have a large value. 
+
+#### The training process is to complete the above calculations and compare it to the expected output. The resulting comparison will be used to adjust the weights being applied to each feature, then this process is repeated many times until the values of the weights reach their steady state values.
+
+## Results
+
+#### I compared the results of this algorithm with 10, 100, 1000, and 10000 iterations. The expected output was one, and the results can be seen below, where as the the number of iterations increased, the closer to the correct output.
+
+![Results](Results.jpg)
+
+#### The final values of the weights with 10000 iterations is seen below. As expected, the value of the first weight is the highest value as the first feature has the largest impact on the output. 
+
+![Synaptic Weights](synaptic_weights.jpg)
+
+#### I also plotted how the value of each weight changed with each iteration, as well as the delta between the weights. The below graphs show the results after 10, 100, 1000, and 10000 iterations. 
+
+### 10 Iterations
+![Synaptic Weights Graph](Iterations=10.png)
+
+### 100 Iterations
+![Synaptic Weights Graph](Iterations=100.png)
+
+### 1000 Iterations
+![Synaptic Weights Graph](Iterations=1000.png)
+
+### 10000 Iterations 
+![Synaptic Weights Graph](Iterations=10000.png)
+
+
--- simple-neural-network.py
+++ simple-neural-network.py
@@ -1,5 +1,7 @@
 from numpy import exp, array, random, dot
 import numpy as np
+from tabulate import tabulate
+import matplotlib.pyplot as plt
 
 def sigmoid(x):
     return 1/(1+exp(-x))
@@ -9,6 +11,7 @@ def sigmoid_derivative(x):
 
 data = np.array([[0,0,1,0],[1,1,1,1],[1,0,1,1],[0,1,1,0]])
 
+
 x = data[:,0:3] #input values
 y = np.array([data[:,3]]).T #output values
 
@@ -17,9 +20,10 @@ random.seed(1) #this saves the randomly generated weights so the starting
 synaptic_weights = 2 * random.random((3, 1)) - 1 #starting weights range between -1 and 1
 
 num_iterations = 10000
+results_table = np.empty([num_iterations+1,7])
+
 
 for iteration in range(num_iterations):
-    
     #input value is adjusted by weight using the dot product
     dot_prod = dot(x,synaptic_weights)
     #the values are normalized with the sigmoid function so the result is between
@@ -32,11 +36,48 @@ for iteration in range(num_iterations):
     adjustment = dot(x.T, error*sigmoid_derivative(output))
     #applying our adjustment to update the synaptic weights
     synaptic_weights += adjustment
+    #saving results to a table
+    weight0 = synaptic_weights[0]
+    delta0 = weight0 - results_table[iteration,1]
+    weight1 = synaptic_weights[1]
+    delta1 = weight1 - results_table[iteration,3]
+    weight2 = synaptic_weights[2]
+    delta2 = weight2 - results_table[iteration,5]
+    results_table[iteration+1] = [iteration,weight0,delta0,weight1,delta1,weight2,delta2]
 
 #testing the results
-test = [1,0,0] #expected result is
+test = [1,0,0] #expected result is 1
 result = sigmoid(dot(test,synaptic_weights))
 print('Result = ',result)
+print_table = results_table.tolist()
+print_table[0] = ['Iteration', 'Weight 1','Delta1','Weight 2','Delta2','Weight 3','Delta3']
+print(tabulate(print_table,headers='firstrow',tablefmt='fancy_grid'))
 
+#plotting data
+x_axis = results_table[:,0]
+plot_weight0 = results_table[:,1]
+plot_delta0 = results_table[:,2]
+plot_weight1 = results_table[:,3]
+plot_delta1 = results_table[:,4]
+plot_weight2 = results_table[:,5]
+plot_delta2 = results_table[:,6]
+
+line1, = plt.plot(x_axis,plot_weight0,color='green')
+line1.set_label('Weight 1')
+line2, = plt.plot(x_axis,plot_delta0,color='green',linestyle='dashed')
+line2.set_label('Delta of Weight 1')
+line3, = plt.plot(x_axis,plot_weight1,color='blue')
+line3.set_label('Weight 2')
+line4, = plt.plot(x_axis,plot_delta1,color='blue',linestyle='dashed')
+line4.set_label('Delta of Weight2')
+line5, = plt.plot(x_axis,plot_weight2,color='pink')
+line5.set_label('Weight 3')
+line6, = plt.plot(x_axis,plot_delta2,color='pink',linestyle='dashed')
+line6.set_label('Delta of Weight 3')
+
+plt.xlabel('Number of Iterations')
+plt.ylabel('Weight/Delta Values')
+plt.legend()
+plt.show()
 
 
